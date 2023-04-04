d32fb6dd66d936e568d894cad120eeb27822eba4
jeongjiyeon315 <jung_j_yeon@naver.com>
2023-04-04 11:49:04 +0900

    feat : 프로젝트 초기 설정
    Resolves : #1

---
+++ src/test/java/yeinyeongha/SMooD/SMooDApplicationTests.java
@@ -0,0 +1,13 @@
+package yeinyeongha.SMooD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SMooDApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

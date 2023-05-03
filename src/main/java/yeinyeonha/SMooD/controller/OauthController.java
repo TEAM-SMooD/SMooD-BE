0aab73eba00b8532c4100d2f50a68604f82a8d8e
jeongjiyeon315 <jung_j_yeon@naver.com>
2023-05-03 20:37:55 +0900

    feat : swagger3 적용

--- src/main/java/yeinyeonha/SMooD/controller/OauthController.java
+++ src/main/java/yeinyeonha/SMooD/controller/OauthController.java
@@ -1,5 +1,6 @@
 package yeinyeonha.SMooD.controller;
 
+import io.swagger.annotations.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,8 +12,16 @@ import yeinyeonha.SMooD.service.OauthService;
 
 @RestController
 @RequiredArgsConstructor
+@Api(tags = "login")
 public class OauthController {
     private final OauthService oauthService;
+    @ApiOperation(
+            value = "카카오 로그인",
+            notes = "카카오 로그인 API")
+    @ApiResponses({
+            @ApiResponse(code=200, message="성공"),
+            @ApiResponse(code=401, message="접근 권한이 없습니다.")
+    })
     @GetMapping("/login/oauth/{provider}")
     public ResponseEntity<LoginResponseDto> login(@PathVariable String provider, @RequestParam String code) {
         LoginResponseDto loginResponse = oauthService.login(provider, code);

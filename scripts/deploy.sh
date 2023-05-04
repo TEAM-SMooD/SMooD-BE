608e0974ba90682e5b7aa74bef4daeb4e49080da
jeongjiyeon315 <jung_j_yeon@naver.com>
2023-05-05 00:55:25 +0900

    feat : deploy.sh 파일 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 BUILD_JAR=$(ls /home/ubuntu/build/libs/SMooD-0.0.1-SNAPSHOT.jar)
 JAR_NAME=$(basename $BUILD_JAR)
+PROJECT_NAME=SMooD-0.0.1-SNAPSHOT
 echo "> build 파일명: $JAR_NAME" >> /home/ubuntu/deploy.log
 
 echo "> build 파일 복사" >> /home/ubuntu/deploy.log
@@ -8,10 +9,10 @@ DEPLOY_PATH=/home/ubuntu/
 cp $BUILD_JAR $DEPLOY_PATH
 
 echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/ubuntu/deploy.log
-CURRENT_PID=$(pgrep -f $JAR_NAME)
+CURRENT_PID=$(pgrep -fl PROJECT_NAME | grep jar | awk '{print $1}')
 
-if [ -z $CURRENT_PID ]
-then
+echo "> pid: $CURRENT_PID"
+if [ -z "$CURRENT_PID" ]; then
   echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ubuntu/deploy.log
 else
   echo "> kill -15 $CURRENT_PID"

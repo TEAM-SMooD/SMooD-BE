20b83a892a325e3dbd0a63c44e9a5e430e5e59f1
jeongjiyeon315 <jung_j_yeon@naver.com>
2023-05-15 20:59:16 +0900

    feat : deploy.sh 파일 변경

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -14,11 +14,11 @@ CURRENT_PID=$(pgrep -fl SMooD | grep java | awk '{print $1}')
 
 echo "> pid: $CURRENT_PID"
 if [ -z $CURRENT_PID ]; then
-    echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
+  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
-    echo "> kill -15 $CURRENT_PID"
-    kill -15 $CURRENT_PID
-    sleep 5
+  echo "> kill -15 $CURRENT_PID"
+  kill -15 $CURRENT_PID
+  sleep 5
 fi
 
 DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME

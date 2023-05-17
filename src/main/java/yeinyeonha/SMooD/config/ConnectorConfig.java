0c0ae3aed81cb40a3f97bcc32f889b44c76f9bc2
jeongjiyeon315 <jung_j_yeon@naver.com>
2023-05-17 14:45:46 +0900

    test : ssl connect 설정

--- src/main/java/yeinyeonha/SMooD/config/ConnectorConfig.java
+++ src/main/java/yeinyeonha/SMooD/config/ConnectorConfig.java
@@ -32,6 +32,7 @@ public class ConnectorConfig {
         Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
         connector.setScheme("http");
         connector.setSecure(true);
+        connector.setPort(8443);
         connector.setRedirectPort(443);
         return connector;
     }

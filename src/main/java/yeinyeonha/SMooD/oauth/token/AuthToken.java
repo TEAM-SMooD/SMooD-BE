2a43d9c2a7987830db17f0de0ebba8fa74c3a922
jeongjiyeon315 <jung_j_yeon@naver.com>
2023-05-24 13:50:49 +0900

    test : jwt token 테스트

--- src/main/java/yeinyeonha/SMooD/oauth/token/AuthToken.java
+++ src/main/java/yeinyeonha/SMooD/oauth/token/AuthToken.java
@@ -56,10 +56,12 @@ public class AuthToken {
 
     public boolean validate() {
         log.info(this.getToken());
+        log.info("여기까지 왔지롱");
         return this.getTokenClaims() != null;
     }
 
     public Claims getTokenClaims() {
+        log.info("그리고 여기차례");
         try {
             return Jwts.parserBuilder()
                     .setSigningKey(key)

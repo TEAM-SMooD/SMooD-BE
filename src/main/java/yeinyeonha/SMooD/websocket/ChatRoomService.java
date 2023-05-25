464c686cbc91da68678f4e68bb8acae10481aac6
jeongjiyeon315 <jung_j_yeon@naver.com>
2023-05-26 01:42:24 +0900

    feat : 채팅방 기능 수정
    - 삭제와 생성시 @Transactional 설정

--- src/main/java/yeinyeonha/SMooD/websocket/ChatRoomService.java
+++ src/main/java/yeinyeonha/SMooD/websocket/ChatRoomService.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.PostConstruct;
 import java.util.*;
@@ -27,13 +28,13 @@ public class ChatRoomService {
         ChatRoom chatRoom = chatRoomRepository.findById(id).get();
         return new ChatRoomResponseDto(chatRoom);
     }
-
+    @Transactional
     //채팅방 생성
     public ChatRoomResponseDto createRoom(ChatRequestDto chatRequestDto) {
         ChatRoom chatRoom = chatRoomRepository.save(ChatRequestDto.toEntity(chatRequestDto));
         return new ChatRoomResponseDto(chatRoom);
     }
-
+    @Transactional
     //채팅방 삭제
     public void deleteRoom(Long chatRoomId) {
         chatRoomRepository.deleteById(chatRoomId);

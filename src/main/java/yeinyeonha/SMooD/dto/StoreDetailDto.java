2666eae30ca0bc62a6527db4c775b1ae23a14da9
jeongjiyeon315 <jung_j_yeon@naver.com>
2023-05-31 18:05:51 +0900

    feat : StoreDetailDto 수정 - name과 긍정리뷰비율 return

--- src/main/java/yeinyeonha/SMooD/dto/StoreDetailDto.java
+++ src/main/java/yeinyeonha/SMooD/dto/StoreDetailDto.java
@@ -8,9 +8,11 @@ import java.util.List;
 
 @Getter
 public class StoreDetailDto {
+    private final String name;
     private final List<String> url;
     private final List<KeywordReviewDto> list;
     private final List<String> topKeyword;
+    private final Double positive;
     private final Double location_x;
     private final Double location_y;
 
@@ -28,9 +30,11 @@ public class StoreDetailDto {
         list3.add(store.getStoreKeywordList().get(0).getKeyword().getName());
         list3.add(store.getStoreKeywordList().get(1).getKeyword().getName());
         list3.add(store.getStoreKeywordList().get(2).getKeyword().getName());
+        this.name = store.getName();
         this.url = list1;
         this.list = list2;
         this.topKeyword = list3;
+        this.positive = store.getPositive();
         this.location_x = store.getLocation_x();
         this.location_y = store.getLocation_y();
     }
